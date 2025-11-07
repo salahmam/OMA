facef4ceac71cc676f3a778848a5e89ba822b7d0
salahmam <121464661+salahmam@users.noreply.github.com>
2025-11-07 05:55:16 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,20 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
+// firebase-config.js (في الجذر /OMA/)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAMANgdrLgoGIjqLZOcS_qyEqCUOVC7gOU",
-    authDomain: "oma0-33cc0.firebaseapp.com",
-    projectId: "oma0-33cc0",
-    storageBucket: "oma0-33cc0.firebasestorage.app",
-    messagingSenderId: "899300774863",
-    appId: "1:899300774863:web:0a81a7f5747ef37d49b8ba",
-    measurementId: "G-5CKWYQT9XZ"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAMANgdrLgoGIjqLZOcS_qyEqCUOVC7gOU",
+  authDomain: "oma0-33cc0.firebaseapp.com",
+  projectId: "oma0-33cc0",
+  storageBucket: "oma0-33cc0.firebasestorage.app",
+  messagingSenderId: "899300774863",
+  appId: "1:899300774863:web:0a81a7f5747ef37d49b8ba",
+  measurementId: "G-5CKWYQT9XZ"
+};
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const db = getFirestore(app);
-  const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-  // ✅ تصدير المتغيرات لاستخدامها في الصفحات الأخرى
-  export { app, auth, db, analytics };
-</script>
+export { app, db, auth };
